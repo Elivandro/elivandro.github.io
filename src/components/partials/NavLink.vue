613a44e56983999c79800c5a8f814a2680ec4ede
Elivandro <Elivandro@hotmail.com.br>
2023-10-11 19:57:49 -0400

    components link and button created

---
+++ src/components/partials/NavLink.vue
@@ -0,0 +1,25 @@
+<template>
+    <a :href="link" :title="title" :target="target" class="font-medium text-gray-600 hover:text-gray-200">
+        {{ title }}
+    </a>
+</template>
+
+<script>
+    export default {
+        name: 'NavLink',
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+            link: {
+                type: String,
+                required: true,
+            },
+            target: {
+                type: String,
+                required: false,
+            }
+        }
+    }
+</script>
---
+++ src/components/partials/PrimaryButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="inline-flex items-center justify-center px-4 py-2 text-base font-medium text-white transition-colors rounded-lg select-none disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-dark-eval-2 hover:bg-slate-600 focus:ring-slate-500 bg-slate-800" :disabled="disabled">
+        {{ title }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "PrimaryButton",
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+            disabled: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    }
+</script>
