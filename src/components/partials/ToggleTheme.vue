a22a9bf37bf56fb9cddda4395b2dd8dc632475fd
Elivandro <Elivandro@hotmail.com.br>
2023-11-12 16:37:35 -0400

    toggledark component, svg icons added

---
+++ src/components/partials/ToggleTheme.vue
@@ -0,0 +1,30 @@
+<template>
+    <a href="#" @click.prevent="toggleDark()">
+        <img :src="iconSrc" class='w-5 h-5' :title="theme ? 'light theme' : 'dark theme'" />
+    </a>
+</template>
+
+<script>
+    import { useDark, useToggle } from '@vueuse/core';
+    import { computed } from 'vue';
+
+    import sunImage from '../../assets/icon/sun-light.svg';
+    import moonImage from '../../assets/icon/moon-light.svg';
+
+    export default {
+        setup() {
+            const isDark = useDark();
+            const toggleDark = useToggle(isDark);
+
+            const theme = computed(() => isDark.value);
+            const iconSrc = computed(() => (theme.value ? sunImage : moonImage));
+    
+            return {
+                theme,
+                toggleDark,
+                iconSrc,
+            };
+        },
+    };
+</script>
+  
