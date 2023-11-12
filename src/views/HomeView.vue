045a80f7a09e57839e9d1d7effba3816a47251d5
Elivandro <Elivandro@hotmail.com.br>
2023-11-12 16:49:07 -0400

    fixing title to link a hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
 
                     <ul class="flex items-end justify-center space-x-4 space-y-16" id="socialmedia-icons">
                         <li class="transition-transform duration-300 transform scale-95 dark:shadown-none hover:scale-105">
-                            <a href="https://www.linkedin.com/in/elivandroeas/" target="_blank">
+                            <a href="https://www.linkedin.com/in/elivandroeas/" target="_blank" title="Linkedin">
                                 <!-- LinkedIn svg -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 48 48">
                                     <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z" />
@@ -22,7 +22,7 @@
                             </a>
                         </li>
                         <li class="transition-transform duration-300 transform scale-95 dark:shadown-none hover:scale-105">
-                            <a href="https://github.com/Elivandro" target="_blank">
+                            <a href="https://github.com/Elivandro" target="_blank" title="Github">
                                 <!-- Github svg -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 rounded-full dark:bg-gray-200" viewBox="0 0 64 64">
                                     <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z" />
@@ -30,7 +30,7 @@
                             </a>
                         </li>
                         <li class="transition-transform duration-300 transform scale-95 dark:shadown-none hover:scale-105">
-                            <a href="https://api.whatsapp.com/send?phone=5569999076818" target="_blank">
+                            <a href="https://api.whatsapp.com/send?phone=5569999076818" target="_blank" title="Whatsapp">
                                 <!-- Whatsapp svg -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 48 48">
                                     <path fill="#fff" d="M4.9,43.3l2.7-9.8C5.9,30.6,5,27.3,5,24C5,13.5,13.5,5,24,5c5.1,0,9.8,2,13.4,5.6C41,14.2,43,18.9,43,24 c0,10.5-8.5,19-19,19c0,0,0,0,0,0h0c-3.2,0-6.3-0.8-9.1-2.3L4.9,43.3z" />
