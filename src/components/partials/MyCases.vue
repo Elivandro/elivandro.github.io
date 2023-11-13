091919da1d0d874d21746f2daad1a8e99efe0516
Elivandro <Elivandro@hotmail.com.br>
2023-11-12 22:34:24 -0400

    fixing images size

--- src/components/partials/MyCases.vue
+++ src/components/partials/MyCases.vue
@@ -1,19 +1,19 @@
 <template>
     <figure class="flex flex-wrap justify-center object-cover object-center gap-4 prose-img:cursor-pointer">
         <a href="https://www.gerenciemei.com.br/" target="_blank" class="duration-300 transform scale-95 opacity-75 transition-ease-in-out hover:opacity-100 hover:scale-100">
-            <img src="@/assets/images/projects/case1.webp" class="shadow-md rounded-xl sm:w-[320px]" alt="imagem do projeto 1" title="Gerencie MEI site para gestão de empresas de pequenos empreendedores" />
+            <img src="@/assets/images/projects/case1.webp" width="320px" height="180px" class="shadow-md rounded-xl" alt="imagem do projeto 1" title="Gerencie MEI site para gestão de empresas de pequenos empreendedores" />
         </a>
         <a href="https://elivandro.github.io/learn-proj/CMLife/" target="_blank" class="duration-300 transform scale-95 opacity-75 transition-ease-in-out hover:opacity-100 hover:scale-100">
-            <img src="@/assets/images/projects/case2.webp" class="shadow-md rounded-xl sm:w-[320px]" alt="imagem do projeto 2" title="Clínica médica Life, exemplo de um site de clínica médica" />
+            <img src="@/assets/images/projects/case2.webp" width="320px" height="180px" class="shadow-md rounded-xl" alt="imagem do projeto 2" title="Clínica médica Life, exemplo de um site de clínica médica" />
         </a>
         <a href="https://elivandro.github.io/learn-proj/PousadaSecreta/" target="_blank" class="duration-300 transform scale-95 opacity-75 transition-ease-in-out hover:opacity-100 hover:scale-100">
-            <img src="@/assets/images/projects/case3.webp" class="shadow-md rounded-xl sm:w-[320px]" alt="imagem do projeto 3" title="Pousada Secreta, exemplo do site de uma pousada com resort" />
+            <img src="@/assets/images/projects/case3.webp" width="320px" height="180px" class="shadow-md rounded-xl" alt="imagem do projeto 3" title="Pousada Secreta, exemplo do site de uma pousada com resort" />
         </a>
         <a href="https://elivandro.github.io/learn-proj/projetoEstagio/" target="_blank" class="duration-300 transform scale-95 opacity-75 transition-ease-in-out hover:opacity-100 hover:scale-100">
-            <img src="@/assets/images/projects/case4.webp" class="shadow-md rounded-xl sm:w-[320px]" alt="imagem do projeto 3" title="Busca de pacotes de viagens e hospedagem, exemplo de site" />
+            <img src="@/assets/images/projects/case4.webp" width="320px" height="180px" class="shadow-md rounded-xl" alt="imagem do projeto 3" title="Busca de pacotes de viagens e hospedagem, exemplo de site" />
         </a>
         <a href="https://elivandro.github.io/learn-proj/AgenciaViagens/" target="_blank" class="duration-300 transform scale-95 opacity-75 transition-ease-in-out hover:opacity-100 hover:scale-100">
-            <img src="@/assets/images/projects/case5.webp" class="shadow-md rounded-xl sm:w-[320px]" alt="imagem do projeto 3" title="Busca de pacotes de viagens e hospedagem, exemplo de site" />
+            <img src="@/assets/images/projects/case5.webp" width="320px" height="180px" class="shadow-md rounded-xl" alt="imagem do projeto 3" title="Busca de pacotes de viagens e hospedagem, exemplo de site" />
         </a>
     </figure>
 </template>
